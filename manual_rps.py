7a1c372609bde4b91dddd7d1fe4d1b5ca52fc9ff
unknown <yusufbroujerdi@outlook.com>
2022-08-31 12:59:38 +0100

    Adds the get_winner and play functions. Fixes up formatting.

--- manual_rps.py
+++ manual_rps.py
@@ -1,10 +1,32 @@
 import random
 
+
 get_computer_choice = lambda: random.choice(['Rock', 'Paper', 'Scissors'])
+get_user_choice = lambda: input('Please choose between Rock, Paper or Scissors: ')
+sign_converter = {'Rock' : 0, 'Paper' : 1, 'Scissors' : 2}
+victory_converter = ['Draw', 'User wins', 'Computer wins']
 
-get_user_choice = lambda: input('Please choose between Rock, Paper or Scissors.')
 
-print('hi')
 
-print(get_computer_choice())
+def get_winner(user_choice, computer_choice):
+    user_number = sign_converter[user_choice]
+    computer_number = sign_converter[computer_choice]
+    return victory_converter[(user_number - computer_number) % 3]
 
+
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    
+    if user_choice not in sign_converter:
+        print("Invalid choice. Game aborted.")
+        return
+    
+    print(f'Computer chose {computer_choice}')
+    print(get_winner(user_choice, computer_choice))
+    
+    
+
+if __name__ == '__main__':
+    play()
