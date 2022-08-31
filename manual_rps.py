944f598a00027b85c7fc1362837788d89cac1dd2
unknown <yusufbroujerdi@outlook.com>
2022-08-31 11:59:13 +0100

    Fixes lambda functions in manual_rps.py

---
+++ manual_rps.py
@@ -0,0 +1,10 @@
+import random
+
+get_computer_choice = lambda: random.choice(['Rock', 'Paper', 'Scissors'])
+
+get_user_choice = lambda: input('Please choose between Rock, Paper or Scissors.')
+
+print('hi')
+
+print(get_computer_choice())
+
